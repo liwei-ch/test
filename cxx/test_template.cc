5afd02d9d9aa0151314b925775e86e043f634d17
liwei-iri <liwei-iri@build17.kill.corp.qihoo.net>
2016-04-21 19:09:19 +0800

    add test

--- cxx/test_template.cc
+++ cxx/test_template.cc
@@ -7,7 +7,19 @@ struct OpNewCreator {
     static T* Create() {
         return new T;
     }
-}
+};
+
+class Widget{
+};
+
+template <template <class Created> class CreationPolicy>
+class WidgetManager : public CreationPolicy< Widget> {};
+
+
+template <template <class> class CreationPolicy = OpNewCreator>
+class WidgetManager2 : public CreationPolicy< Widget> {};
+
+typedef WidgetManager<OpNewCreator> MyWidgetMgr;
 
 int main() {
 
